c3aa8a7cf9e76a9c6c2c2d690b5c09718a043975
unknown <diegoivan1987@outlook.com>
2020-05-14 20:19:38 -0500

    se modifico la DATAMEMORY, ya se puede escribir y leer

--- DATAPATH/DATAMEMORY.v
+++ DATAPATH/DATAMEMORY.v
@@ -1,22 +1,24 @@
 module DATAMEMORY(
-	input [31:0]address,
-	input memWrite,
-	input memRead,
+	input [31:0] writeData,address,
+	input memWrite,memRead,
 	output reg [31:0] readData
 );
 
 reg [31:0] mem [0:1023];
 
 
-always @(address)
+always @(address) 
 	begin
-		if(memWrite && (memRead == 0))
-		begin//se escribe
-			readData = mem[address];
+		if(memWrite)
+		begin//se escribe y lee
+			mem[address]=writeData;
+			readData=mem[address];//no borrar ni modificar
+			readData=mem[address];//ni esta, por alguna razon evitan que el registro se convierta en RAM
 		end
 		else
+		if(memRead)
 		begin//solo se lee
-			readData = mem[address];
+			readData=mem[address];
 		end
 	end
 
--- DATAPATH/DATAMEMORY_TB.v
+++ DATAPATH/DATAMEMORY_TB.v
@@ -6,18 +6,23 @@ reg memRead;
 reg memWrite;
 wire [31:0]readData;
 
-DATAMEMORY DUV(.address(address),
-	.memWrite(memWrite),
-	.memRead(memRead),
+DATAMEMORY DUV(.writeData(writeData),.address(address),
+	.memWrite(memWrite),.memRead(memRead),
 	.readData(readData));
 	
 initial
 begin
 	$readmemb("datamemoryTest.txt",DUV.mem);
 	memRead = 1'b1;
-	memWrite = 1'b0;
 	address = 32'd0;
 	#10;
+	memRead = 1'b0;
+	memWrite = 1'b1;
+	address = 32'd1;
+	writeData = 32'd100;
+	#10;
+	memRead = 1'b1;
+	memWrite = 1'b0;
 	address = 32'd1;
 	#10;
 	address = 32'd2;
