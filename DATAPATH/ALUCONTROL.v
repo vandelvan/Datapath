8e2a7b6bf9a03610b448d2f7ab3689cb4b4b302a
Piloalucard <piloalucard@gmail.com>
2020-05-15 18:47:20 -0500

    UC,ALU,Alucontrol listas

--- DATAPATH/ALUCONTROL.v
+++ DATAPATH/ALUCONTROL.v
@@ -14,20 +14,23 @@ begin
 		4'b0010://tipo r
 		begin
 			case(opf)//func = selector
-				6'b000000: ops = 4'b1101;//no operation
+				6'b000000: ops = 4'b1111;//no operation
 				6'b100100: ops = 4'b0000;//&
 				6'b100101: ops = 4'b0001;//|
 				6'b100000: ops = 4'b0010;//+
+				6'b100001: ops = 4'b0010;//+U
 				6'b011011: ops = 4'b0011;//%
 				6'b100111: ops = 4'b0100;//nor
 				6'b000010: ops = 4'b0101;//*
 				6'b100010: ops = 4'b0110;//-
+				6'b100011: ops = 4'b0110;//-U
 				6'b011010: ops = 4'b1000;// /
-				6'b101010: ops = 4'b1001;//slt?
+				6'b101010: ops = 4'b1001;//slt
+				6'b101011: ops = 4'b1001;//sltU
 				default: ops = 4'b1111;//x
 			endcase
 		end
-		4'b0011://branchGTZ, no importa func
+		4'b0011://branchGEZ, no importa func
 			ops=4'b1011;//a >= 0
 		4'b0100://andi
 			ops=4'b0000;
@@ -35,10 +38,14 @@ begin
 			ops=4'b0001;
 		4'b0110://slti
 			ops=4'b1001;
-		4'b0111://slti
+		4'b0111://xori
 			ops=4'b0111;
 		4'b1000://b
 			ops=4'b1111;
+		4'b1001://lui
+			ops=4'b1110;
+		4'b1011://bne
+			ops=4'b1101;
 		4'b1111://bitswap
 			ops=4'b1100;
 		default: ops=4'b1111;
--- DATAPATH/UC.v
+++ DATAPATH/UC.v
@@ -26,6 +26,18 @@ begin
 			branch = 1'b0;
 			aluOp = 4'b0010;
 		end
+	6'b001111://lui
+		begin
+			regDst = 1'b0;
+			aluSrc = 1'b1;
+			jump=1'b0;
+			memtoReg = 1'b0;
+			regWrite = 1'b1;
+			memRead = 1'b0;
+			memWrite = 1'b0;
+			branch = 1'b0;
+			aluOp = 4'b1001;
+		end
 	6'b011111://bitswap
 		begin
 			regDst = 1'b1;
@@ -52,6 +64,18 @@ begin
 		end
 
 	6'b001000://instruccion addi
+		begin
+			regDst = 1'b0;
+			aluSrc = 1'b1;
+			jump=1'b0;
+			memtoReg = 1'b0;
+			regWrite = 1'b1;
+			memRead = 1'b0;
+			memWrite = 1'b0;
+			branch = 1'b0;
+			aluOp = 4'b0000;
+		end
+	6'b001001://instruccion addiU
 		begin
 			regDst = 1'b0;
 			aluSrc = 1'b1;
@@ -123,7 +147,19 @@ begin
 			branch = 1'b0;
 			aluOp = 4'b0110;
 		end
-	6'b000100://instruccion b
+	6'b001011://instruccion sltiU
+		begin
+			regDst = 1'b0;
+			aluSrc = 1'b1;
+			jump=1'b0;
+			memtoReg = 1'b0;
+			regWrite = 1'b1;
+			memRead = 1'b0;
+			memWrite = 1'b0;
+			branch = 1'b0;
+			aluOp = 4'b0110;
+		end
+	6'b000011://instruccion b
 		begin
 			regDst = 1'bx;
 			aluSrc = 1'b0;
@@ -147,7 +183,7 @@ begin
 			branch = 1'b1;
 			aluOp = 4'b0001;
 		end
-	6'b000001://instruccion bgtz
+	6'b000001://instruccion bgez
 		begin
 			regDst = 1'bx;
 			aluSrc = 1'b0;
@@ -158,6 +194,18 @@ begin
 			memWrite = 1'b0;
 			branch = 1'b1;
 			aluOp = 4'b0011;
+		end
+	6'b000101://instruccion bne
+		begin
+			regDst = 1'bx;
+			aluSrc = 1'b0;
+			jump=1'b0;
+			memtoReg = 1'bx;
+			regWrite = 1'b0;
+			memRead = 1'b0;
+			memWrite = 1'b0;
+			branch = 1'b1;
+			aluOp = 4'b1011;
 		end
 	
 	6'b000010://instruccion jump
